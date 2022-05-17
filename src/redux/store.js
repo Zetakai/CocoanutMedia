786434065a6c11f6f944370eb3457e6e44fcddc0
TheCocoanut <32837828+Zetakai@users.noreply.github.com>
2022-05-18 00:47:05 +0700

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import reducer from './../../src/redux/reducer';
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: [''],
+  whitelist: ['watchlist'],
   blacklist: [''],
   timeout: null,
 };
